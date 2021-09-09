d11836417a80654a502ff18615ad04b6f7bc5db3
Daniela Castro Salazar <88462620+DaniCastro-developer@users.noreply.github.com>
2021-09-09 09:47:50 -0300

    cambios en JS codigo cesar completo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,31 @@
 const cipher = {
-  // ...
-};
 
-export default cipher;
+    encode: function (mensaje1,offset) {
+        let menReturn =''; 
+        for (let i=0; i < mensaje1.length; i++) {
+            let numAscii = mensaje1.charCodeAt(i);
+                let position = (numAscii-32+offset)%255+32;
+                menReturn += String.fromCharCode(position);
+            }
+
+    return menReturn;
+    },
+    
+    decode: function(mensaje1,offset) {
+        let menReturn =''; 
+        for (let i=0; i < mensaje1.length; i++) {
+            let numAscii = mensaje1.charCodeAt(i);
+                let position = (numAscii-32-offset)%255+32;
+                menReturn += String.fromCharCode(position);
+            }
+    
+    return menReturn;
+        }
+    
+    };
+    
+    
+    export default cipher;
+    
+    
+    
--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
+
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+//botón limpiar
+
+document.getElementById('limpiar').addEventListener('click', function() {
+  document.getElementsByClassName('texto')[0].value = '';
+  document.getElementsByClassName('texto')[1].value = '';
+  document.getElementsByClassName('texto')[2].value = '';
+});
+
+//botón codificar
+
+document.getElementById('codificar').addEventListener('click', () => {
+  let texto = document.getElementById('mensaje1').value;
+  let clave = parseInt(document.getElementById('offset').value);
+  document.getElementById('mensaje2').value = cipher.encode(texto,clave);
+});
+
+//boton decodificar
+
+document.getElementById('decodificar').addEventListener('click', () => {
+  let texto = document.getElementById('mensaje1').value;
+  let clave = parseInt(document.getElementById('offset').value);
+  document.getElementById('mensaje2').value = cipher.decode(texto,clave);
+});
+
 
